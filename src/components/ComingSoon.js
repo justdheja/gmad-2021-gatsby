9af0ef6a5a5a835b9e241a2642a7289d3d4f1137
justdheja <justdheja@gmail.com>
2021-06-14 21:42:03 +0700

    add animation coming soon

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -4,10 +4,12 @@ const ComingSoon = () => {
   return (
     <div>
       <div className="min-h-screen flex items-center justify-center bg-timer p-8 pt-20 text-white">
-        <div className="text-center">
-          <h2 className="text-6xl title-timer">We Are</h2>
-          <h2 className="text-6xl uppercase poppins">Coming Soon!</h2>
-        </div>
+        <Fade bottom>
+          <div className="text-center">
+            <h2 className="text-6xl title-timer">We Are</h2>
+            <h2 className="text-6xl uppercase poppins">Coming Soon!</h2>
+          </div>
+        </Fade>
       </div>
     </div>
   )
