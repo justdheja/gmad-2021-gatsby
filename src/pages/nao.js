08cac3f714741e0bf74eda4637c819fcb259d110
justdheja <justdheja@gmail.com>
2021-06-20 17:22:27 +0700

    fix btn download nao

--- src/pages/nao.js
+++ src/pages/nao.js
@@ -81,12 +81,12 @@ const Nao = () => {
               href="https://drive.google.com/drive/folders/1QQNa7ZMMXlUby9yq9Cs2iHoofJCaO2YP?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
-              className="w-full"
+              className="p-2 md:ml-0 font-semibold cursor-pointer bg-palette-pink hover:bg-palette-dusty shadow-effect-nao text-sm text-white transition ease-in duration-200 text-center text-base w-6/12 shadow-lg rounded-full"
             >
-              <button className="p-2 md:ml-0 font-semibold cursor-pointer bg-palette-pink hover:bg-palette-dusty shadow-effect-nao text-sm text-white transition ease-in duration-200 text-center text-base w-6/12 shadow-lg rounded-full">
+              {/* <button className="p-2 md:ml-0 font-semibold cursor-pointer bg-palette-pink hover:bg-palette-dusty shadow-effect-nao text-sm text-white transition ease-in duration-200 text-center text-base w-6/12 shadow-lg rounded-full"> */}
                 Download Booklet
                 {/* <i>Coming Soon</i> */}
-              </button>
+              {/* </button> */}
             </a>
           </div>
 
