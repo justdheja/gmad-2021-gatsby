2a444fbc3de7db8cfcdeed1f8f2a8babc1ffcd61
justdheja <justdheja@gmail.com>
2021-08-14 17:07:48 +0700

    feat: improve accessibility score

--- src/components/Events.js
+++ src/components/Events.js
@@ -17,13 +17,13 @@ function Events() {
           <div className="flex p-0 w-full h-56 justify-center orange-text-container">
             <div className="flex flex-col justify-center text-left px-8 w-4/6 h-full text-white ">
               <Link to="/a-talk">
-                <img src={atalk} className="w-64 md:pl-4 mb-2" />
+                <img src={atalk} alt="atalk logo" className="w-64 md:pl-4 mb-2" />
               </Link>
               Atalk is a national accounting seminar and talk show that is intended <br /> for students, business people, civil servant, and the general public
             </div>
             <img
               src={fotoAtalk}
-              alt=""
+              alt="A-Talk Picture"
               className="h-full px-4"
             />
           </div>
@@ -33,12 +33,12 @@ function Events() {
           <div className="flex p-0 w-full h-56 justify-center pink-text-container">
             <img
               src={fotoNao}
-              alt=""
+              alt="Nao Picture"
               className="h-full px-4"
             />
             <div className="flex flex-col justify-center items-end text-right px-8 w-4/6 h-full text-white ">
               <Link to="/nao">
-                <img src={nao} className="w-64 md:pl-4 mb-2" />
+                <img src={nao} alt="nao logo" className="w-64 md:pl-4 mb-2" />
               </Link>
               NAO is one of the biggest and most prestigious <br />
               accounting competition for undergraduates in Indonesia
@@ -50,13 +50,13 @@ function Events() {
           <div className="flex p-0 w-full h-56 green-text-container justify-center">
             <div className="flex flex-col justify-center text-left px-8 w-4/6 h-full text-white ">
               <Link to="/jnc">
-                <img src={jnc} className="w-64 md:pl-4 mb-2" />
+                <img src={jnc} alt="jnc logo" className="w-64 md:pl-4 mb-2" />
               </Link>
               JNC is the event expansion in GMAD, that encompasses the aim <br /> to create an inclusive in paper research concept
             </div>
             <img
               src={fotoJnc}
-              alt=""
+              alt="JNC Picture"
               className="h-full px-4"
             />
           </div>
--- src/components/EventsMobile.js
+++ src/components/EventsMobile.js
@@ -16,13 +16,13 @@ function EventsMobile() {
           <div className="p-0 w-full py-4">
             <div className="justify-center text-justify px-8 h-full text-white ">
               <Link to="/a-talk">
-                <img src={atalk} className="w-64 md:pl-4 mb-2 mx-auto" />
+                <img src={atalk} alt="atalk logo" className="w-64 md:pl-4 mb-2 mx-auto" />
               </Link>
               Atalk is a national accounting seminar and talk show that is
               intended for students, business people, civil servant, and
               the general public
             </div>
-            <img src={fotoAtalk} alt="" className="px-4 my-2" />
+            <img src={fotoAtalk} alt="atalk picture" className="px-4 my-2" />
           </div>
         </Fade>
 
@@ -30,11 +30,11 @@ function EventsMobile() {
           <div className="w-full py-4">
             <div className=" px-8 h-full text-justify text-white ">
               <Link to="/nao">
-                <img src={nao} className="w-64 md:pl-4 mb-2 mx-auto" />
+                <img src={nao} alt="nao logo" className="w-64 md:pl-4 mb-2 mx-auto" />
               </Link>
               NAO is one of the biggest and most prestigious accounting competition for undergraduates in Indonesia
             </div>
-            <img src={fotoNao} alt="" className="px-4 my-2" />
+            <img src={fotoNao} alt="nao picture" className="px-4 my-2" />
           </div>
         </Fade>
 
@@ -42,11 +42,11 @@ function EventsMobile() {
           <div className="py-4 w-full justify-center">
             <div className="flex-col justify-center text-justify px-8 h-full text-white ">
               <Link to="/jnc">
-                <img src={jnc} className="w-64 md:pl-4 mb-2 mx-auto" />
+                <img src={jnc} alt="jnc logo" className="w-64 md:pl-4 mb-2 mx-auto" />
               </Link>
               JNC is the event expansion in GMAD, that encompasses the aim  to create an inclusive in paper research concept
             </div>
-            <img src={fotoJnc} alt="" className="px-4 my-2" />
+            <img src={fotoJnc} alt="jnc picture" className="px-4 my-2" />
           </div>
         </Fade>
       </div>
